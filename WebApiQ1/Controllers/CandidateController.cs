c4b12928b316a24d705534e886baf84c8104e5fe
PragyaVyas <120946186+PragyaVyas@users.noreply.github.com>
2023-10-29 18:09:16 +1000

    Solution : 1st

---
+++ WebApiQ1/Controllers/CandidateController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiQ1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCandidateInfo()
+        {
+            var candidateInfo = new
+            {
+                name = "test",
+                phone = "test"
+            };
+            return Ok(candidateInfo);
+        }
+    }
+}
